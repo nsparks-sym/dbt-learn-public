bce0a55fb3f4d30321e8c781c0aacb5a0c142643
Nathan Sparks <nathan.sparks@symend.com>
2021-02-04 13:25:56 +0000

    renamed customer model

--- tests/fct_orders_amount_pos.sql
+++ tests/fct_orders_amount_pos.sql
@@ -1,6 +1,6 @@
 SELECT  order_id
         , SUM(amountDollars) AS totalAmount
-FROM    {{ ref('stg_payments')}}
+FROM    {{ ref('fct_orders')}}
 WHERE   1=1
 GROUP BY order_id
 HAVING NOT(totalAmount >= 0)
